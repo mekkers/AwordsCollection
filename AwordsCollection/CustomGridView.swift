b483024b3719319097e15dbbf00f59d4c929ec6d
mekkers <dev.mekk@gmail.com>
2022-01-27 09:13:28 +0300

    add itemSize

--- AwordsCollection/CustomGridView.swift
+++ AwordsCollection/CustomGridView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct CustomGridView<Content, T>: View where Content: View {
     let items: [T]
     let columns: Int
-    let content: (T) -> Content
+    let content: (CGFloat, T) -> Content
     
     var rows: Int {
         items.count / columns
@@ -18,15 +18,14 @@ struct CustomGridView<Content, T>: View where Content: View {
     
     var body: some View {
         GeometryReader { geometry in
-            let sideSize = geometry.size.width / CGFloat(columns)
+            let itemSize = geometry.size.width / CGFloat(columns)
             ScrollView {
                 VStack {
                     ForEach(0...rows, id: \.self) { rowIndex in
                         HStack {
                             ForEach(0..<columns) { columnIndex in
-                                if let index = indexFor(row: rowIndex, column: columnIndex) {
-                                content(items[index])
-                                        .frame(width: sideSize, height: sideSize)
+                                if let index = elementRow(row: rowIndex, column: columnIndex) {
+                                content(itemSize, items[index])
                                 } else {
                                     Spacer()
                                 }
@@ -38,7 +37,7 @@ struct CustomGridView<Content, T>: View where Content: View {
         }
     }
     
-    private func indexFor(row: Int, column: Int) -> Int? {
+    private func elementRow(row: Int, column: Int) -> Int? {
         let index = row * columns + column
         return index < items.count ? index : nil
     }
@@ -46,8 +45,9 @@ struct CustomGridView<Content, T>: View where Content: View {
 
 struct CustomGridView_Previews: PreviewProvider {
     static var previews: some View {
-        CustomGridView(items: [11, 3, 7, 17, 5, 2, 1], columns: 3) { item in
+        CustomGridView(items: [11, 3, 7, 17, 5, 2, 1], columns: 3) { itemSize, item in
             Text("\(item)")
+                .frame(width: itemSize, height: itemSize)
         }
     }
 }
